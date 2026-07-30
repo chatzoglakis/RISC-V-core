7005b009faddf893506fb0ac8718d19317cf3047
Michael <mihalishatzoglakis@gmail.com>
2026-07-30 16:34:58 +0300

    Modify RAM entity for 32-bit data and address width

--- design/ram.vhd
+++ design/ram.vhd
@@ -5,24 +5,21 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ram is
     generic (
-        DATA_WIDTH: integer := 8;
-        ADDRESS_WIDTH: integer := 14 --16KB total memory size
+        ADDRESS_WIDTH: integer := 12
     );
 
     port(
         clk: in STD_LOGIC;
-        we: in STD_LOGIC;
+        we: in STD_LOGIC_VECTOR(4 downto 0); --each bit corresponds to one word byte
         address: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
-        data_in: in STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-
-        --4 byte/word burst as output
-        data_out: out STD_LOGIC_VECTOR((DATA_WIDTH*4)-1 downto 0)
+        data_in: in STD_LOGIC_VECTOR(31 downto 0);
+        data_out: out STD_LOGIC_VECTOR(31 downto 0)
     );
 end ram;
 
 architecture Behavioral of ram is
 
-    type ram_type is array(0 to (2**ADDRESS_WIDTH)-1) of STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    type ram_type is array(0 to (2**ADDRESS_WIDTH)-1) of STD_LOGIC_VECTOR(31 downto 0);
     signal ram: ram_type;
 
 begin
@@ -30,10 +27,22 @@ begin
     process(clk)
     begin
         if rising_edge(clk) then
-            data_out <= ram(to_integer(unsigned(address)));
+            data_out <= ram(to_integer(unsigned(address))); 
 
-            if we = '1' then
-                ram(to_integer(unsigned(address))) <= data_in;
+            if we(0) = '1' then
+                ram(to_integer(unsigned(address)))(7 downto 0) <= data_in(7 downto 0);
+            end if;
+
+            if we(1) = '1' then
+                ram(to_integer(unsigned(address)))(15 downto 8) <= data_in(15 downto 8);
+            end if;
+
+            if we(2) = '1' then
+                ram(to_integer(unsigned(address)))(23 downto 16) <= data_in(23 downto 16);
+            end if;
+
+            if we(3) = '1' then
+                ram(to_integer(unsigned(address)))(31 downto 24) <= data_in(31 downto 24);
             end if;
         end if;
     end process;
