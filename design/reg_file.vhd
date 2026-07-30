c20bbb6160817de610a30eed749f4b2d43df0c26
Michael <mihalishatzoglakis@gmail.com>
2026-07-30 03:31:02 +0300

    Add register file

---
+++ design/reg_file.vhd
@@ -0,0 +1,42 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity reg_file is
+    generic (
+        ADDRESS_WIDTH: integer := 5;
+        DATA_WIDTH: integer := 32     
+    );
+
+    port (
+        clk: in STD_LOGIC;
+        rs1: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
+        rs2: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
+        rd: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
+        we: in STD_LOGIC;
+        data_in: in STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+        data_out1: out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+        data_out2: out STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0)
+    );
+end reg_file;
+
+architecture Behavioral of reg_file is
+
+    type reg_array_type is array (0 to (2**ADDRESS_WIDTH) - 1) of STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    signal reg_file: reg_array_type;
+
+begin
+
+    data_out1 <= reg_file(to_integer(unsigned(rs1)));
+    data_out2 <= reg_file(to_integer(unsigned(rs2)));
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if we = '1' then
+                reg_file(to_integer(unsigned(rd))) <= data_in;
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
