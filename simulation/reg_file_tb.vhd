1f06cb9163c8761119eb104d633895736276ea99
Michael <mihalishatzoglakis@gmail.com>
2026-08-05 02:08:54 +0300

    Add x0 check

--- simulation/reg_file_tb.vhd
+++ simulation/reg_file_tb.vhd
@@ -16,8 +16,8 @@ architecture Behavioral of reg_file_tb is
     signal rd: STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
     signal we: STD_LOGIC;
     signal data_in: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-    signal data_out1: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-    signal data_out2: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    signal reg_out1: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    signal reg_out2: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
 
 begin
 
@@ -29,8 +29,8 @@ begin
             rd => rd,
             we => we,
             data_in => data_in,
-            data_out1 => data_out1,
-            data_out2 => data_out2
+            reg_out1 => reg_out1,
+            reg_out2 => reg_out2
         );
 
     clk_proc: process
@@ -46,20 +46,30 @@ begin
         report "Testing read/write";
         we <= '1';
         data_in <= x"FFFFFFFF";
-        rd <= "00000";
-        rs1 <= "00000";
-        rs2 <= "00000";
+        rd <= "00001";
+        rs1 <= "00001";
+        rs2 <= "00001";
         wait for 100 ns;
-        assert data_out1 = x"FFFFFFFF" and data_out2 = x"FFFFFFFF" report "Error in reading/writing" severity error;
+        assert reg_out1 = x"FFFFFFFF" and reg_out2 = x"FFFFFFFF" report "Error in reading/writing" severity error;
 
         report "Testing write enable";
         we <= '0';
         data_in <= x"21F7B1A0";
+        rd <= "00001";
+        rs1 <= "00001";
+        rs2 <= "00001";
+        wait for 100 ns;
+        assert reg_out1 = x"FFFFFFFF" and reg_out2 = x"FFFFFFFF" report "Error in write enable" severity error;
+
+        report "Testing register x0";
+        we <='1';
+        data_in <= x"0F320910";
         rd <= "00000";
         rs1 <= "00000";
-        rs2 <= "00000";
         wait for 100 ns;
-        assert data_out1 = x"FFFFFFFF" and data_out2 = x"FFFFFFFF" report "Error in write enable" severity error;
+        assert reg_out1 = x"00000000" report "ERROR IN REGISTER x0" severity error;
+
+        report "Simulation complete" severity failure;
 
     end process;
 
