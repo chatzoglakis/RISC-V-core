b5e8aec9c63d122aab684d350b9035aee357efd6
Michael <mihalishatzoglakis@gmail.com>
2026-08-07 03:40:46 +0300

    Modify RAM write enable and initialize with test code
    Updated the write enable signal to 4 bits and initialized RAM with test machine code.

--- design/ram.vhd
+++ design/ram.vhd
@@ -10,7 +10,7 @@ entity ram is
 
     port(
         clk: in STD_LOGIC;
-        we: in STD_LOGIC_VECTOR(4 downto 0); --each bit corresponds to one word byte
+        we: in STD_LOGIC_VECTOR(3 downto 0); --each bit corresponds to one word byte
         address: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
         data_in: in STD_LOGIC_VECTOR(31 downto 0);
         data_out: out STD_LOGIC_VECTOR(31 downto 0)
@@ -20,14 +20,25 @@ end ram;
 architecture rtl of ram is
 
     type ram_type is array(0 to (2**ADDRESS_WIDTH)-1) of STD_LOGIC_VECTOR(31 downto 0);
-    signal ram: ram_type;
+    -- Initialize the RAM with compiled machine code for testing purposes
+    signal ram: ram_type := (
+        0 => x"00000093", -- PC = 0:  addi x1, x0, 0
+        1 => x"00500113", -- PC = 4:  addi x2, x0, 5
+        2 => x"00108093", -- PC = 8:  addi x1, x1, 1  <-- LOOP START
+        3 => x"02102023", -- PC = 12: sw   x1, 32(x0)
+        4 => x"fe209ce3", -- PC = 16: bne  x1, x2, -8
+        5 => x"0000006f", -- PC = 20: jal  x0, 0      <-- END PROGRAM
+        
+        -- The rest of the 16KB memory is filled with zeros
+        others => x"00000000"
+    );
 
 begin
 
     process(clk)
     begin
+        data_out <= ram(to_integer(unsigned(address))); 
         if rising_edge(clk) then
-            data_out <= ram(to_integer(unsigned(address))); 
 
             if we(0) = '1' then
                 ram(to_integer(unsigned(address)))(7 downto 0) <= data_in(7 downto 0);
