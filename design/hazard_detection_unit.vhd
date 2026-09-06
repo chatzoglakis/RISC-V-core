41a80c3c3db151027ed0ed0ca88b81487d339942
Michael <mihalishatzoglakis@gmail.com>
2026-09-06 05:46:04 +0300

    Refactor hazard detection logic with LUI check
    Refactor hazard detection unit to detect load-use hazards that start with a LUI instruction

--- design/hazard_detection_unit.vhd
+++ design/hazard_detection_unit.vhd
@@ -3,11 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity hazard_detection_unit is
     port(
-        new_opcode: in STD_LOGIC_VECTOR(4 downto 0);
         IF_ID_opcode: in STD_LOGIC_VECTOR(4 downto 0);
-        IF_ID_rd: in STD_LOGIC_VECTOR(4 downto 0);
-        new_rs1: in STD_LOGIC_VECTOR(4 downto 0);
-        new_rs2: in STD_LOGIC_VECTOR(4 downto 0);
+        ID_EX_opcode: in STD_LOGIC_VECTOR(4 downto 0);
+        ID_EX_rd: in STD_LOGIC_VECTOR(4 downto 0);
+        IF_ID_rs1: in STD_LOGIC_VECTOR(4 downto 0);
+        IF_ID_rs2: in STD_LOGIC_VECTOR(4 downto 0);
 
         stall_pipeline: out STD_LOGIC
     );
@@ -15,11 +15,20 @@ end hazard_detection_unit;
 
 architecture rtl of hazard_detection_unit is
 
+    constant S_TYPE: STD_LOGIC_VECTOR(4 downto 0) := "01000";
+    constant B_TYPE: STD_LOGIC_VECTOR(4 downto 0) := "11000";
+    constant R_TYPE: STD_LOGIC_VECTOR(4 downto 0) := "01100";
+    constant LOAD: STD_LOGIC_VECTOR(4 downto 0) := "00000";
+    constant LUI: STD_LOGIC_VECTOR(4 downto 0) := "01101";
+
 begin
 
     process(all)
     begin
-        if IF_ID_opcode = "00000" and (IF_ID_rd = new_rs1 or ( IF_ID_rd = new_rs2 and (new_opcode = "01100" or new_opcode = "11000" or new_opcode = "01000"))) and IF_ID_rd /= "00000" then
+        if (ID_EX_opcode = LOAD or ID_EX_opcode = LUI) and 
+           (ID_EX_rd = IF_ID_rs1 or ( ID_EX_rd = IF_ID_rs2 and (IF_ID_opcode = R_TYPE or IF_ID_opcode = B_TYPE or IF_ID_opcode = S_TYPE))) 
+           and ID_EX_rd /= "00000" then
+
             stall_pipeline <= '1';
         else
             stall_pipeline <= '0';
