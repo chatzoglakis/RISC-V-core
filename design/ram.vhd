c6d57d249d9cce84b575f6007753943dc3aa06cd
Michael <mihalishatzoglakis@gmail.com>
2026-09-12 04:44:30 +0300

    Add enable input for reading

--- design/ram.vhd
+++ design/ram.vhd
@@ -10,54 +10,45 @@ entity ram is
 
     port(
         clk: in STD_LOGIC;
-        we: in STD_LOGIC_VECTOR(3 downto 0); --each bit corresponds to one word byte
+        en: STD_LOGIC;
+        write_en: in STD_LOGIC_VECTOR(3 downto 0); --each bit corresponds to one word byte
         address: in STD_LOGIC_VECTOR(ADDRESS_WIDTH-1 downto 0);
         data_in: in STD_LOGIC_VECTOR(31 downto 0);
         data_out: out STD_LOGIC_VECTOR(31 downto 0)
     );
 end ram;
 
-architecture rtl of ram is
+architecture Behavioral of ram is
 
     type ram_type is array(0 to (2**ADDRESS_WIDTH)-1) of STD_LOGIC_VECTOR(31 downto 0);
-    -- Initialize the RAM with compiled machine code for testing purposes
-    signal ram: ram_type := (
-        0 => x"00000093", -- PC = 0:  addi x1, x0, 0
-        1 => x"00300113", -- PC = 4:  addi x2, x0, 3
-        2 => x"00108093", -- PC = 8:  addi x1, x1, 1  <-- LOOP START
-        3 => x"06102223", -- PC = 12: sw   x1, 100(x0)
-        4 => x"fe209ce3", -- PC = 16: bne  x1, x2, -8 (Jump to PC 8)
-        5 => x"06402183", -- PC = 20: lw   x3, 100(x0)
-        6 => x"00018233", -- PC = 24: add  x4, x3, x0
-        7 => x"0000006f", -- PC = 28: jal  x0, 0      <-- END PROGRAM
-        
-        others => x"00000000"
-    );
+
+    signal ram: ram_type := (others => x"00000000";
 
 begin
     
     process(clk)
     begin
         if rising_edge(clk) then
+            if en = '1' then
+                data_out <= ram(to_integer(unsigned(address))); 
 
-            data_out <= ram(to_integer(unsigned(address))); 
+                if write_en(0) = '1' then
+                    ram(to_integer(unsigned(address)))(7 downto 0) <= data_in(7 downto 0);
+                end if;
 
-            if we(0) = '1' then
-                ram(to_integer(unsigned(address)))(7 downto 0) <= data_in(7 downto 0);
+                if write_en(1) = '1' then
+                    ram(to_integer(unsigned(address)))(15 downto 8) <= data_in(15 downto 8);
+                end if;
+
+                if write_en(2) = '1' then
+                    ram(to_integer(unsigned(address)))(23 downto 16) <= data_in(23 downto 16);
+                end if;
+
+                if write_en(3) = '1' then
+                    ram(to_integer(unsigned(address)))(31 downto 24) <= data_in(31 downto 24);
+                end if;
             end if;
+    end if;
+end process;
 
-            if we(1) = '1' then
-                ram(to_integer(unsigned(address)))(15 downto 8) <= data_in(15 downto 8);
-            end if;
-
-            if we(2) = '1' then
-                ram(to_integer(unsigned(address)))(23 downto 16) <= data_in(23 downto 16);
-            end if;
-
-            if we(3) = '1' then
-                ram(to_integer(unsigned(address)))(31 downto 24) <= data_in(31 downto 24);
-            end if;
-        end if;
-    end process;
-
-end rtl;
+end Behavioral;
