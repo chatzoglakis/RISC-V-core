9bc0d1da8d126b2a79734c11664fdbe30a237e08
Michael <mihalishatzoglakis@gmail.com>
2026-09-20 23:12:27 +0300

    Update program loader to construct 32 bit instruction from each byte

--- design/program_loader.vhd
+++ design/program_loader.vhd
@@ -17,6 +17,10 @@ architecture rtl of program_loader is
 
     signal done: STD_LOGIC;
     signal address_num: integer := 0;
+    signal byte: STD_LOGIC_VECTOR(7 downto 0);
+    signal assemble_reg: STD_LOGIC_VECTOR(31 downto 0);
+    signal done_delayed: STD_LOGIC := '0';
+    signal byte_count: integer range 0 to 3 := 0;
 
 begin
 
@@ -25,7 +29,7 @@ begin
         clk => clk,
         rx => rx,
         done => done,
-        data => instruction
+        data => byte
     );
 
     process (all)
@@ -34,15 +38,30 @@ begin
             if prog_mode = '0' then
                 we <= "0000";
                 address_num <= 0;
+                byte_count <= 0;
+                done_delayed <= '0';
             else
+                done_delayed <= done;
                 we <= "0000";
-                if done = '1' then
-                    we <= "1111";
+
+                if we = "1111" then
                     address_num <= address_num + 1;
                 end if;
+
+                if done = '1' and done_delayed = '0' then
+                    assemble_reg <= byte & assemble_reg(31 downto 8);
+
+                    if byte_count = 3 then
+                        byte_count <= 0;
+                        we <= "1111";
+                    else
+                        byte_count <= byte_count + 1;
+                    end if;
+                end if;    
             end if;
         end if;
     end process;
+    instruction <= assemble_reg;
     address <= STD_LOGIC_VECTOR(to_unsigned(address_num, 12));
 
 end rtl;
