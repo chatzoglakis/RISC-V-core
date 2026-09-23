10e37815f2bf2d6630ffd8153cc1fe33016efd5d
Michael <mihalishatzoglakis@gmail.com>
2026-09-24 00:42:17 +0300

    Refactor forwarding logic in VHDL unit
    Deleted variables due to simulation issues and enforced forwarding check order through nested if statements

--- design/forwarding_unit.vhd
+++ design/forwarding_unit.vhd
@@ -41,30 +41,22 @@ architecture rtl of forwarding_unit is
 begin
 
     process(all)
-        variable forwarded_A_in_EX_MEM: std_logic := '0';
-        variable forwarded_B_in_EX_MEM: std_logic := '0';
     begin
         forward_A <= "00";
         forward_B <= "00";
 
-        if EX_MEM_reg_we = '1' and EX_MEM_rd /= "00000" then
-            if EX_MEM_rd = ID_EX_rs1 and has_rs1(ex_stage_opcode) then
+        if has_rs1(ex_stage_opcode) then
+            if (EX_MEM_reg_we = '1' and EX_MEM_rd /= "00000" and EX_MEM_rd = ID_EX_rs1) then
                 forward_A <= "01";
-                forwarded_A_in_EX_MEM := '1';
-            end if;
-
-            if EX_MEM_rd = ID_EX_rs2 and has_rs2(ex_stage_opcode) then
-                forward_B <= "01";
-                forwarded_B_in_EX_MEM := '1';
+            elsif (MEM_WB_reg_we = '1' and MEM_WB_rd /= "00000" and MEM_WB_rd = ID_EX_rs1) then
+                forward_A <= "10";
             end if;
         end if;
 
-        if MEM_WB_reg_we = '1' and MEM_WB_rd /= "00000" then
-            if MEM_WB_rd = ID_EX_rs1 and has_rs1(ex_stage_opcode) and forwarded_A_in_EX_MEM = '0' then
-                forward_A <= "10";
-            end if;
-
-            if MEM_WB_rd = ID_EX_rs2 and has_rs2(ex_stage_opcode) and forwarded_B_in_EX_MEM = '0' then
+        if has_rs2(ex_stage_opcode) then
+            if (EX_MEM_reg_we = '1' and EX_MEM_rd /= "00000" and EX_MEM_rd = ID_EX_rs2) then
+                forward_B <= "01";
+            elsif (MEM_WB_reg_we = '1' and MEM_WB_rd /= "00000" and MEM_WB_rd = ID_EX_rs2) then
                 forward_B <= "10";
             end if;
         end if;
